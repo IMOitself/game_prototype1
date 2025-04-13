8fca6035fbf01ce9cd76f62975b7b8767653a713
IMOitself <russjonn@gmail.com>
2025-04-13 14:00:05 +0800

    init

---
+++ app/src/main/java/imo/game_prototype1/MainActivity.java
@@ -0,0 +1,14 @@
+package imo.game_prototype1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
